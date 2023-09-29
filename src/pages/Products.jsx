5699d8d04fd8ce191dd60c4fda557bdc67a60901
Antonio Prado <antonioeprado18@gmail.com>
2023-09-29 13:23:31 -0300

    feat: products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,16 +4,18 @@ import ProductComponent from "../components/Product/ProductComponent";
 import styled from "styled-components";
 import NoProductsMessage from "../components/NoProductsMessage/Message";
 import { useProducts } from "../hooks/useProducts";
-import ReactModal from "react-modal";
-import ProductModal from "../components/ProductModal/ProductModal";
+import { MdAddCircle } from "react-icons/md";
+import AddProduct from "../components/AddProduct/AddProduct";
+import useWindowDimensions from "../hooks/useWindow";
 
 function Products() {
     const [products, setProducts] = useState();
     const [trigger, setTrigger] = useState(false);
     const [modalIsOpen, setIsOpen] = useState(false);
-
     const token = useToken();
     const { productsFetch } = useProducts();
+    const windowDimensions = useWindowDimensions();
+    const { width } = windowDimensions;
 
     useEffect(() => {
         productsFetch(token)
@@ -24,7 +26,7 @@ function Products() {
     }, [trigger]);
 
     function handleOpen() {
-        setIsOpen(true);
+        setIsOpen(!modalIsOpen);
     }
 
     if (!products) {
@@ -32,18 +34,39 @@ function Products() {
     }
 
     return (
-        <ComponentWrapper id='modal-main'>
+        <ComponentWrapper sWidth={width}>
             <SectionTitle>
-                <h2>GROCERIES LIST</h2>
+                <h3>GROCERIES</h3>
             </SectionTitle>
-            {products.length === 0 && <NoProductsMessage />}
-            {products.map((product) => (
-                <ProductComponent key={product.id} reload={{ trigger, setTrigger }} {...product} />
-            ))}
-            <AddButton onClick={handleOpen}>
-                <ion-icon name='add-outline'></ion-icon>
-            </AddButton>
-            <ProductModal token={token} modalState={{ modalIsOpen, setIsOpen }} reload={{ trigger, setTrigger }} />
+            {products.length === 0 ? (
+                <NoProductsMessage />
+            ) : (
+                <Table>
+                    <thead>
+                        <tr>
+                            <DateHeader>DATE</DateHeader>
+                            <ProductHeader>PRODUCT</ProductHeader>
+                            <UserHeader>USER</UserHeader>
+                            <ActionHeader>ACTIONS</ActionHeader>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((product) => (
+                            <ProductComponent key={product.id} reload={{ trigger, setTrigger }} {...product} />
+                        ))}
+                    </tbody>
+                </Table>
+            )}
+            <AddWrapper>
+                <AddProduct
+                    isOpen={modalIsOpen}
+                    setIsOpen={setIsOpen}
+                    token={token}
+                    trigger={trigger}
+                    setTrigger={setTrigger}
+                />
+                <AddButton onClick={handleOpen} />
+            </AddWrapper>
         </ComponentWrapper>
     );
 }
@@ -51,30 +74,68 @@ function Products() {
 const ComponentWrapper = styled.div`
     display: flex;
     flex-flow: column nowrap;
-    padding-left: 80px;
-    height: 100%;
-    position: relative;
+    padding-left: ${(props) => (props.sWidth >= 714 ? "80px" : "0px")};
 `;
 
 const SectionTitle = styled.header`
-    margin-bottom: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 15px;
     font-size: 24px;
 `;
 
-const AddButton = styled.div`
+const AddWrapper = styled.div`
     display: flex;
     align-items: center;
-    justify-content: center;
+    position: relative;
+    height: 100px;
+    width: 55%;
+    align-self: flex-end;
+`;
+
+const AddButton = styled(MdAddCircle)`
     position: absolute;
-    right: 240px;
-    bottom: 180px;
-    width: 60px;
-    height: 60px;
+    right: 15px;
+    width: 40px;
+    height: 40px;
     border-radius: 50%;
     background-color: ${(props) => props.theme.terciaryColor};
     color: ${(props) => props.theme.mainColor};
-    font-size: 48px;
     cursor: pointer;
+`;
+
+const DateHeader = styled.th`
+    column-span: 1;
+    width: 80px;
+`;
+const ProductHeader = styled.th`
+    column-span: 3;
+`;
+const UserHeader = styled.th`
+    column-span: 2;
+`;
+
+const ActionHeader = styled.th`
+    column-span: 1;
+    width: 80px;
+`;
+const Table = styled.table`
+    border: ${(props) => `1px solid ${props.theme.textColor}`};
+    border-radius: 7px;
+    thead {
+        height: 40px;
+        background-color: ${(props) => props.theme.terciaryColor};
+        ${DateHeader} {
+            border-radius: 7px 0 0 0;
+        }
+        ${ActionHeader} {
+            border-radius: 0 7px 0 0;
+        }
+    }
+    tbody tr {
+        height: 80px;
+    }
 `;
 
 export default Products;
