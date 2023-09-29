d17d911fc5f510bc799e643066dbb763064a9189
Antonio Prado <antonioeprado18@gmail.com>
2023-09-29 13:23:15 -0300

    feat: homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import Menu from "../components/Menu/Menu";
 import styled from "styled-components";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+import useToken from "../hooks/useToken";
+import useWindowDimensions from "../hooks/useWindow";
+import MobileMenu from "../components/Menu/MobileMenu";
 
 function Homepage() {
+    const token = useToken();
+    const userData = jwtDecode(token);
+    const windowDimensions = useWindowDimensions();
+    const location = useLocation();
+
     return (
         <ComponentWrapper>
-            <Menu />
-            <MainContent>
+            {windowDimensions.width >= 714 && <Menu />}
+            <MainContent sWidth={windowDimensions.width}>
+                {windowDimensions.width >= 714 && location.pathname === "/home" && (
+                    <h2>Welcome back, {userData && userData.name}</h2>
+                )}
                 <Outlet />
+                {windowDimensions.width < 714 && <MobileMenu />}
             </MainContent>
         </ComponentWrapper>
     );
@@ -24,7 +37,8 @@ const ComponentWrapper = styled.div`
 `;
 
 const MainContent = styled.main`
-    width: 80%;
+    width: ${(props) => (props.sWidth >= 714 ? "80%" : "100%")};
+    height: 100%;
 `;
 
 export default Homepage;
